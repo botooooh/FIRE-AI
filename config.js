ec76c0968284d7443f02de275d185ac565d836d3
botooooh <botcompte763@gmail.com>
2024-12-17 05:29:43 +0000

    Update config.js

--- config.js
+++ config.js
@@ -70,9 +70,9 @@ global.APIKeys = {
 
 // Sticker WM
 global.botname = process.env.BOTNAME
-global.premium = 'false'
-global.packname = 'FIRE MD'
-global.author = 'OPAKSFIRE'
+global.premium = 'true'
+global.packname = '𝕴𝖛𝖆𝖓×͜× •|• FIRE_MD'
+global.author = '𝕴𝖛𝖆𝖓×͜×'
 global.menuvid = 'https://files.catbox.moe/r9oil6.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/\n'
 global.dygp = 'https://whatsapp.com/channel/0029VajYjMJJf05aRQXKx82W'
